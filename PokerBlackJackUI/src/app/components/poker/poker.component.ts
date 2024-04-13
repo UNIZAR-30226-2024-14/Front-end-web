327e65ec1c3bc1aed6a1d8f0bd09973647742a87
EnesGurbuz1 <enesbugragurbuz1@gmail.com>
2024-04-13 20:09:26 +0200

    add poker requirements for hand

--- PokerBlackJackUI/src/app/components/poker/poker.component.ts
+++ PokerBlackJackUI/src/app/components/poker/poker.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { PokerService } from './services/poker.service';
+import { HandRank } from './models/hand.model';
+import { PairService } from './services/pair.service';
 
 @Component({
   selector: 'app-poker',
@@ -6,7 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poker.component.scss', '../../../styles.scss'],
 })
 export class PokerComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private pokerService: PokerService,
+    private pairService : PairService
+  ) {}
 
-  ngOnInit(): void {}
+  playerHand: any[] = [];
+  tableHand: any[] = [];
+  combinedCards: any[] = [];
+  evulatedHands: HandRank = 0;
+  handRankMessage: string = 'null';
+
+
+  ngOnInit(): void {
+    this.playerHand = this.pokerService.getRandomPlayerHand();
+    this.tableHand = this.pokerService.getRandomTableCards();
+    this.combinedCards = this.playerHand.concat(this.tableHand);
+    this.handRankMessage = this.pairService.evaluateHand(this.combinedCards);
+  }
 }
