4aa07487ba6507e718e9a6e1c94be864d30f32b7
EnesGurbuz1 <enesbugragurbuz1@gmail.com>
2024-04-13 20:08:22 +0200

    add card model and hand model

---
+++ PokerBlackJackUI/src/app/components/poker/models/hand.model.ts
@@ -0,0 +1,12 @@
+export enum HandRank {
+    HighCard,
+    OnePair,
+    TwoPairs,
+    ThreeOfAKind,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOfAKind,
+    StraightFlush,
+    RoyalFlush
+  }
