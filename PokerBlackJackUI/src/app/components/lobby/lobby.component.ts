4398ea1878af2d2c4db23428c957dd17edc53328
iaydemirankabeta <theangelo112@gmail.com>
2024-04-16 18:00:44 +0200

    improve blackjack lobby user interface

--- PokerBlackJackUI/src/app/components/lobby/lobby.component.ts
+++ PokerBlackJackUI/src/app/components/lobby/lobby.component.ts
@@ -12,21 +12,17 @@ export class LobbyComponent implements OnInit {
   numberOfPlayersSelected!: number;
   availablePlayers!: Player[];
 
-  //injecting the main game service
   constructor(
     private blackjackService: BlackjackService,
     private chatService: ChatService
   ) {}
 
   ngOnInit(): void {
-    //check if there are some players available
     let playersFromService = this.blackjackService.getPlayers();
     if (playersFromService !== undefined && playersFromService.length) {
       this.availablePlayers = playersFromService;
       this.numberOfPlayersSelected = this.availablePlayers.length;
-      //change some properties for each available player
       for (let player of this.availablePlayers) {
-        //put it in edit mode
         player.gameModeOn = false;
         player.cards = [];
         player.currentBetValue = 0;
@@ -46,7 +42,6 @@ export class LobbyComponent implements OnInit {
     if (target instanceof HTMLSelectElement) {
       const numberOfPlayers = parseInt(target.value, 10);
       if (!isNaN(numberOfPlayers)) {
-        // Şimdi numberOfPlayers değişkeni güvenli bir şekilde kullanılabilir
         this.availablePlayers = [];
         this.numberOfPlayersSelected = numberOfPlayers;
         for (let i = 0; i < numberOfPlayers; i++) {
@@ -69,13 +64,21 @@ export class LobbyComponent implements OnInit {
     }
   }
 
-  //this method validates the name and bankroll for each player and then calls the game service to start a new game
+  isGameStartValid(): boolean {
+    for (let player of this.availablePlayers) {
+      if (!player.name || player.bankroll <= 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   startGame(): void {
     const playerNames: string[] = this.availablePlayers.map(
       (player) => player.name
     );
     this.chatService.setUsernames(playerNames);
-    console.log(playerNames); // Tüm oyuncu isimlerinin dizisi
+    console.log(playerNames);
     this.blackjackService.startGame(this.availablePlayers);
   }
 }
